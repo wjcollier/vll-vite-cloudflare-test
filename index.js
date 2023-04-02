691f5169ae9df22fe6b3d4cf48e46e16b2e8869c
Billy_Joe <williamjcollier4@gmail.com>
2023-04-02 10:02:09 +0800

    cloudflare nameservers-turnstile setup

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const explicitRenderHtml = require('./index.html')
+import explicitRenderHtml from './index.html'
+// import dotenv from 'dotenv'
+
 require('dotenv').config()
-// This is the demo secret key. In prod, we recommend you store
-// your secret key(s) safely.
+
 const SECRET_KEY = process.env.SECRET_KEY
+
 async function handlePost(request) {
   const body = await request.formData()
   // Turnstile injects a token in "cf-turnstile-response".
@@ -26,14 +28,12 @@ async function handlePost(request) {
       'The provided Turnstile token was not valid! \n' + JSON.stringify(outcome)
     )
   }
-  // The Turnstile token was successfully validated. Proceed with your application logic.
-  // Validate login, redirect user, etc.
-  // For this demo, we just echo the "/siteverify" response:
+ 
   return new Response(
     'Turnstile token successfully validated. \n' + JSON.stringify(outcome)
   )
 }
-export default {
+export default  {
   async fetch(request) {
     if (request.method === 'POST') {
       return await handlePost(request)
@@ -42,20 +42,21 @@ export default {
     if (url.pathname === '/explicit') {
       const body = await explicitRenderHtml()
       return new Response(body, {
-        headers: { 'Content-Type': 'text/html' },
-      })
+				headers: { 'Content-Type': 'text/html' },
+			})
     }
-    // Check if the request is for the root domain and return the HTML with the Turnstile widget.
+  
     if (url.pathname === '/') {
-      const html = await explicitRenderHtml()
-      const response = new Response(html, {
-        headers: { 'Content-Type': 'text/html' },
-      })
-      response.headers.set('Cache-Control', 'no-store')
-      response.headers.set('Pragma', 'no-cache')
-      return response
-    }
-    // The request is for a static asset, so just pass it through to the origin.
+			const html = await explicitRenderHtml()
+			const response = new Response(html, {
+				headers: { 'Content-Type': 'text/html' },
+			})
+			response.headers.set('Cache-Control', 'no-store')
+			response.headers.set('Pragma', 'no-cache')
+			return response
+		}
+
+   
     return fetch(request)
   },
 }
